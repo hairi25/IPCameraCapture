47f2bd2c53c05cf63f74844b24d1b1fc8eb0b202
khairibprima <khairi@billionprima.com.my>
2023-08-18 10:12:55 +0800

    Add project files.

---
+++ IPCameraCapture/FormTest.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace IPCameraCapture
+{
+    partial class FormTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.tbUname = new System.Windows.Forms.TextBox();
+            this.tbPword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(11, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(301, 177);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(107, 269);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(75, 23);
+            this.btnCapture.TabIndex = 1;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Location = new System.Drawing.Point(73, 195);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(239, 20);
+            this.tbUrl.TabIndex = 2;
+            // 
+            // tbUname
+            // 
+            this.tbUname.Location = new System.Drawing.Point(73, 221);
+            this.tbUname.Name = "tbUname";
+            this.tbUname.Size = new System.Drawing.Size(109, 20);
+            this.tbUname.TabIndex = 3;
+            // 
+            // tbPword
+            // 
+            this.tbPword.Location = new System.Drawing.Point(73, 247);
+            this.tbPword.Name = "tbPword";
+            this.tbPword.Size = new System.Drawing.Size(109, 20);
+            this.tbPword.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Url";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Password";
+            // 
+            // FormTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 304);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPword);
+            this.Controls.Add(this.tbUname);
+            this.Controls.Add(this.tbUrl);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "FormTest";
+            this.Text = "Test";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.TextBox tbUname;
+        private System.Windows.Forms.TextBox tbPword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ IPCameraCapture/FormTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IPCameraCapture
+{
+    public partial class FormTest : Form
+    {
+        private readonly HttpClient _httpClient;
+        private const string CameraImageUrl = "http://192.168.1.9/axis-cgi/jpg/image.cgi";
+        private const string CameraUname = "root";
+        private const string CameraPword = "root";
+
+        public FormTest()
+        {
+            InitializeComponent();
+            _httpClient = new HttpClient();
+            tbUrl.Text = CameraImageUrl;
+            tbUname.Text = CameraUname;
+            tbPword.Text = CameraPword;
+        }
+
+        private Image downloadedImage;
+        private async void btnCapture_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var credentials = new NetworkCredential( CameraUname, CameraPword); // Replace with actual username and password
+                var handler = new HttpClientHandler { Credentials = credentials };
+
+                using (var httpClient = new HttpClient(handler))
+                using (var response = await httpClient.GetAsync(CameraImageUrl))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        using (var stream = await response.Content.ReadAsStreamAsync())
+                        {
+                            if (downloadedImage != null) 
+                            {
+                                downloadedImage.Dispose();
+                            }
+                            downloadedImage = Image.FromStream(stream);
+                            pictureBox.Image = downloadedImage;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to download the image.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }   
+    }
+}
